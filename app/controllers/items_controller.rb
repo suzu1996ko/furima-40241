6f6a7becebd004ad537dc42fd80ae4066b4b568b
suzu1996ko <0tj6892738t256x@gmail.com>
2024-05-22 20:44:26 +0900

    商品出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,9 +7,19 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
   private
 
-  def message_params
-    params.require(:item).permit(:image)
+  def item_params
+    params.require(:item).permit(:name, :item_details, :category_id, :state_id, :price, :postage_id, :prefecture_id,
+                                 :shipping_date_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,20 @@
 class Item < ApplicationRecord
+  validates :name, presence: true
+  validates :item_details, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :state_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true
+  validates :postage_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :image, presence: true
+
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :shipping_date
 end
