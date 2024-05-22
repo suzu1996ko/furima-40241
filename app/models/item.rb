0404d53a74e88f908bb6137e3968843b7a7539a2
suzu1996ko <0tj6892738t256x@gmail.com>
2024-05-22 21:35:38 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
-  validates :name, presence: true
-  validates :item_details, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :item_details, presence: true, length: { maximum: 1000 }
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :state_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
+                    format: { with: /\A[0-9]+\z/ }
   validates :postage_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
