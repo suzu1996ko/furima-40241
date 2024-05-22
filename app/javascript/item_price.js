b0b9feb5956b2dd223e4f812a202a7a324a4da72
suzu1996ko <0tj6892738t256x@gmail.com>
2024-05-23 03:12:32 +0900

    販売手数料と販売利益の表示

---
+++ app/javascript/item_price.js
@@ -0,0 +1,9 @@
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  const addTaxDom = document.getElementById("add-tax-price");
+  const tax = Math.floor(inputValue * 0.1)
+    addTaxDom.innerHTML = tax;
+  const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(inputValue - tax);
+})
