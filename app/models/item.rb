0bb6b3e19f2904bdbca307134c49100cedbadf60
suzu1996ko <0tj6892738t256x@gmail.com>
2024-05-23 20:00:18 +0900

    商品出品機能の修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :item_details, presence: true, length: { maximum: 1000 }
   validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :state_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :postage_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :prefecture_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_date_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
