db24e13f895afebe7c4cda75e2de178eecfd4634
suzu1996ko <0tj6892738t256x@gmail.com>
2024-06-11 21:20:47 +0900

    購入機能のモデルやコントローラーの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :purchase_records
 
   validates :name, presence: true
   validates :birthday, presence: true
