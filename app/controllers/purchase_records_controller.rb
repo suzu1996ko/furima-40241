81d6e0e0c06e9d0e7315672ad64d7104ba56c969
suzu1996ko <0tj6892738t256x@gmail.com>
2024-06-15 19:05:09 +0900

    決済機能の実装

--- app/controllers/purchase_records_controller.rb
+++ app/controllers/purchase_records_controller.rb
@@ -1,5 +1,6 @@
 class PurchaseRecordsController < ApplicationController
   def index
+    gon.public_key = ENV['PAYJP_PUBLIC_KEY']
     @purchase_address = PurchaseAddress.new
     @item = Item.find(params[:item_id])
   end
@@ -7,10 +8,12 @@ class PurchaseRecordsController < ApplicationController
   def create
     @purchase_address = PurchaseAddress.new(purchase_params)
     if @purchase_address.valid?
+      pay_item
       @purchase_address.save
       redirect_to root_path
     else
       @item = Item.find(params[:item_id])
+      gon.public_key = ENV['PAYJP_PUBLIC_KEY']
       render :index, status: :unprocessable_entity
     end
   end
@@ -19,6 +22,15 @@ class PurchaseRecordsController < ApplicationController
 
   def purchase_params
     params.require(:purchase_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :building_name,
-                                             :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+                                             :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token]) 
   end
+end
+
+def pay_item
+  Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+  Payjp::Charge.create(
+    amount: Item.find(params[:item_id]).price,
+    card: purchase_params[:token],
+    currency: 'jpy'
+  )
 end
--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,10 +1,13 @@
 class PurchaseAddress
+  attr_accessor :token
+
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :municipality, :house_number, :building_name, :phone_number
 
   with_options presence: true do
     validates :user_id
     validates :item_id
+    validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :municipality
     validates :house_number
