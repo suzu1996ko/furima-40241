54d6db81820c4ca6dd48be7ba9da8615b6a1b11e
suzu1996ko <0tj6892738t256x@gmail.com>
2024-06-17 00:28:23 +0900

    purchase_addressモデルの単体テストコードの修正

--- spec/factories/purchase_addresses.rb
+++ spec/factories/purchase_addresses.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_address do
     post_code { '123-4567' }
-    prefecture_id { 1 }
+    prefecture_id { 2 }
     municipality { '東京都' }
     house_number { '1-1' }
     building_name { '東京ハイツ' }
