03db4a4fdfce9eabcfeb042b0fa4c12a0cfb8670
suzu1996ko <0tj6892738t256x@gmail.com>
2024-06-17 00:24:39 +0900

    prefecture_idのバリデーションの変更

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -8,11 +8,11 @@ class PurchaseAddress
     validates :item_id
     validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality
     validates :house_number
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
 
   def save
     purchase_record = PurchaseRecord.create(user_id:, item_id:)
